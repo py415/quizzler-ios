16dffec13fa182e91e2a783f77ab7d38df008d20
Philip Yu <pyu3@mail.sfsu.edu>
2019-06-29 19:06:35 -0700

    Quizzler displays questions, accepts answer input, and traverses through question bank

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -10,44 +10,84 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    //Place your instance variables here
-    
-    
+    // Outlets
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet var progressBar: UIView!
     @IBOutlet weak var progressLabel: UILabel!
     
+    // Properties
+    let allQuestions = QuestionBank()
+    var pickedAnswer: Bool = false
+    var questionNumber: Int = 0
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         
-    }
-
-
-    @IBAction func answerPressed(_ sender: AnyObject) {
-  
-    }
-    
-    
-    func updateUI() {
-      
-    }
-    
-
-    func nextQuestion() {
+        let firstQuestion = allQuestions.list[questionNumber]
+        questionLabel.text = firstQuestion.questionText
         
     }
     
+    @IBAction func answerPressed(_ sender: AnyObject) {
+        
+        if sender.tag == 1 {
+            pickedAnswer = true
+        } else if sender.tag == 2 {
+            pickedAnswer = false
+        } else {
+            print("ERROR! Selected answer is invalid or does not exist!")
+        }
+        
+        checkAnswer()
+        
+        questionNumber += 1
+        
+        nextQuestion()
+        
+    }
+    
+    func updateUI() {
+        
+        
+        
+    }
+    
+    func nextQuestion() {
+        
+        if questionNumber < allQuestions.list.count {
+            questionLabel.text = allQuestions.list[questionNumber].questionText
+        } else {
+            print("End of Quiz!")
+            let alert = UIAlertController(title: "Awesome!", message: "You've finished all the questions, do you want to start over?", preferredStyle: .alert)
+            let restartAction = UIAlertAction(title: "Restart", style: .default) { (UIAlertAction) in
+                self.startOver()
+            }
+            
+            alert.addAction(restartAction)
+            present(alert, animated: true, completion: nil)
+        }
+        
+    }
     
     func checkAnswer() {
         
+        let correctAnswer = allQuestions.list[questionNumber].answer
+        
+        if correctAnswer == pickedAnswer {
+            print("Question #\(questionNumber). Correct!")
+        } else {
+            print("Question #\(questionNumber). Wrong!")
+        }
+        
     }
-    
     
     func startOver() {
-       
+        
+        questionNumber = 0
+        nextQuestion()
+        
     }
-    
-
     
 }
